19cee6dceab0c0f5a4225cfc826f7cdabc2f7b32
İhsan Algül <algulihsan@hotmail.com>
2022-12-26 22:50:08 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import csv
+
+def update_graph(num):
+    plt.cla() # Clear the previous graph
+    plt.plot(data[:num]) # Plot the new data
+    return line,
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.set_title('Heart Rate')
+ax.set_xlabel('Time')
+ax.set_ylabel('BPM')
+
+file = open('data.csv')
+
+type(file)
+
+csvreader = csv.reader(file)
+
+data = []
+for row in csvreader:
+        data.append(row)
+data
+
+ani = animation.FuncAnimation(fig, update_graph, frames=range(len(data)), interval=200)
+
+# plt.show()
+
+ani.save('heart_rate.gif', writer='imagemagick')
